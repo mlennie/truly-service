707eac5099f9dccc29d9a0c41e935e58c76c0e16
Monty Lennie <montylennie@gmail.com>
2018-07-30 20:01:38 +0000

    add csv file reading error handling

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const express = require('express')
 const csv = require('fast-csv')
 const app = express()
+const fileName = "interview-callerid-data.csv"
 
 app.use(cors())
 app.use(express.json());
@@ -10,35 +11,45 @@ app.use(express.urlencoded());
 
 app.get('/query', (req, res) => {
   let results = []
-  csv
-   .fromPath("interview-callerid-data.csv")
-    .on("data", function(data){
-      if (data[0] === req.query.number) {
-        results.push({
-          name: data[2],
-          number: data[0],
-          context: data[1]
-        })
-      }
-    })
-    .on("end", function(){
-      res.json({results: results})
-    });
+  try {
+    csv
+     .fromPath(fileName)
+      .on("data", function(data){
+        if (data[0] === req.query.number) {
+          results.push({
+            name: data[2],
+            number: data[0],
+            context: data[1]
+          })
+        }
+      })
+      .on("end", function(){
+        res.json({results: results})
+      });
+  } catch(error) {
+    // log error event here
+    res.status(400).send("There was an issue querying. Please try again soon")
+  }
 })
 
 app.post('/number', (req, res) => {
-  csv
-   .fromPath("interview-callerid-data.csv")
-    .on("data", function(data){
-      if (data[0] === req.body.number &&
-          data[1] === req.body.context) {
-        res.status(400).send("Number with that context already exists").end();
-      }
-    })
-    .on("end", function(){
-      // Persist new number record/object here
-      res.send("Number added")
-    });
+  try {
+    csv
+     .fromPath(fileName)
+      .on("data", function(data){
+        if (data[0] === req.body.number &&
+            data[1] === req.body.context) {
+          res.status(400).send("Number with that context already exists").end();
+        }
+      })
+      .on("end", function(){
+        // Persist new number record/object here
+        res.send("Number added")
+      });
+  } catch(error) {
+    // log error event here
+    res.status(400).send("There was an issue adding number. Please try again soon")
+  }
 })
 
 var port = process.env.PORT
